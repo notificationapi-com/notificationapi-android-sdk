80550f04d4fafca851a79c2d2e2d2bf09652a603
Devin <devthedevel@gmail.com>
2023-06-18 13:41:07 -0230

    Update MainActivity.kt

--- app/src/main/java/com/notificationapi/example/MainActivity.kt
+++ app/src/main/java/com/notificationapi/example/MainActivity.kt
@@ -2,10 +2,21 @@ package com.notificationapi.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.notificationapi.notificationapi_android_sdk.NotificationApi
+import com.notificationapi.notificationapi_android_sdk.models.NotificationApiCredentials
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        NotificationApi.initialize(context = this)
+
+        NotificationApi.shared.configure(
+            NotificationApiCredentials(
+            clientId = "2vgja235ahrfqc4n8hjfol06ur",
+            userId = "androidTest"
+            )
+        )
     }
 }
