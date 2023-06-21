252fa6884e8d1900faf60f0429ee2aa71e582274
Devin <devthedevel@gmail.com>
2023-06-21 14:36:28 -0230

    Update NotificationApiDeviceInfo.kt

--- notificationapi_android_sdk/src/main/java/com/notificationapi/notificationapi_android_sdk/models/NotificationApiDeviceInfo.kt
+++ notificationapi_android_sdk/src/main/java/com/notificationapi/notificationapi_android_sdk/models/NotificationApiDeviceInfo.kt
@@ -1,7 +1,6 @@
 package com.notificationapi.notificationapi_android_sdk.models
 
 import android.content.Context
-import com.google.android.gms.ads.identifier.AdvertisingIdClient
 
 class NotificationApiDeviceInfo internal constructor(context: Context) {
     val app_id: String
@@ -13,7 +12,6 @@ class NotificationApiDeviceInfo internal constructor(context: Context) {
 
     init {
         app_id = context.packageName
-//        ad_id = AdvertisingIdClient.getAdvertisingIdInfo(context).id.toString()
         ad_id = ""
         device_id = ""
         platform = "Android"
