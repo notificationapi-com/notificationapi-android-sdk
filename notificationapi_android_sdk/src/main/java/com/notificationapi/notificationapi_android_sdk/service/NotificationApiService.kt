202ae8ee8946defc55a5949ac1bffc0b8a1c1c7e
Devin <devthedevel@gmail.com>
2023-06-21 14:10:02 -0230

    Update NotificationApiService.kt

--- notificationapi_android_sdk/src/main/java/com/notificationapi/notificationapi_android_sdk/service/NotificationApiService.kt
+++ notificationapi_android_sdk/src/main/java/com/notificationapi/notificationapi_android_sdk/service/NotificationApiService.kt
@@ -2,7 +2,6 @@ package com.notificationapi.notificationapi_android_sdk.service
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
-import com.google.firebase.messaging.RemoteMessage
 import com.notificationapi.notificationapi_android_sdk.NotificationApi
 import com.notificationapi.notificationapi_android_sdk.models.NotificationApiError
 import com.notificationapi.notificationapi_android_sdk.repositories.TokenRepository
@@ -20,8 +19,6 @@ open class NotificationApiService: FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         super.onNewToken(token)
 
-        Log.d(NotificationApi.TAG, "FCM Token: $token")
-
         CoroutineScope(Dispatchers.IO).launch {
             try {
                 TokenRepository().syncToken(token)
@@ -29,11 +26,5 @@ open class NotificationApiService: FirebaseMessagingService() {
                 Log.w(NotificationApi.TAG, e.message!!)
             }
         }
-    }
-
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
-
-        Log.d(NotificationApi.TAG, "Got message: ${message.notification?.title}")
     }
 }
